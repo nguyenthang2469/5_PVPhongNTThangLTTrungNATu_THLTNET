5c52bfb62e5bf4ddb8d5fc51c1cdc5b73b824796
nguyenthang2469 <nguyenthangcbq@gmail.com>
2023-11-04 11:47:17 +0700

    Sửa form thông tin nhóm thành user control

--- QuanLyThuVien/fQuanly.Designer.cs
+++ QuanLyThuVien/fQuanly.Designer.cs
@@ -46,18 +46,9 @@ namespace QuanLyThuVien
             this.nhàXuấtBảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mượnTrảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.báoCáoThốngKêToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pnThongtinnhaphattrien = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.thôngTinNhómToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnMain = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
-            this.pnThongtinnhaphattrien.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -67,7 +58,8 @@ namespace QuanLyThuVien
             this.tàiKhoảnToolStripMenuItem,
             this.danhMucjToolStripMenuItem,
             this.mượnTrảToolStripMenuItem,
-            this.báoCáoThốngKêToolStripMenuItem});
+            this.báoCáoThốngKêToolStripMenuItem,
+            this.thôngTinNhómToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 2, 0, 2);
@@ -195,117 +187,36 @@ namespace QuanLyThuVien
             this.báoCáoThốngKêToolStripMenuItem.Size = new System.Drawing.Size(170, 29);
             this.báoCáoThốngKêToolStripMenuItem.Text = "Báo cáo thống kê";
             // 
-            // pnThongtinnhaphattrien
+            // thôngTinNhómToolStripMenuItem
             // 
-            this.pnThongtinnhaphattrien.Controls.Add(this.label8);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label9);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label6);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label7);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label4);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label5);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label3);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label2);
-            this.pnThongtinnhaphattrien.Controls.Add(this.label1);
-            this.pnThongtinnhaphattrien.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnThongtinnhaphattrien.Location = new System.Drawing.Point(0, 33);
-            this.pnThongtinnhaphattrien.Margin = new System.Windows.Forms.Padding(4);
-            this.pnThongtinnhaphattrien.Name = "pnThongtinnhaphattrien";
-            this.pnThongtinnhaphattrien.Size = new System.Drawing.Size(772, 387);
-            this.pnThongtinnhaphattrien.TabIndex = 1;
+            this.thôngTinNhómToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thôngTinNhómToolStripMenuItem.Name = "thôngTinNhómToolStripMenuItem";
+            this.thôngTinNhómToolStripMenuItem.Size = new System.Drawing.Size(159, 29);
+            this.thôngTinNhómToolStripMenuItem.Text = "Thông tin nhóm";
+            this.thôngTinNhómToolStripMenuItem.Click += new System.EventHandler(this.thôngTinNhómToolStripMenuItem_Click);
             // 
-            // label1
+            // pnMain
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(145, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(479, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Thông tin thành viên - Nhóm 5";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(411, 97);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(257, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Họ tên: Nguyễn Thiện Thắng";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(102, 97);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(236, 24);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Mã sinh viên: 20103100683";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(102, 138);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(236, 24);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Mã sinh viên: 20103100689";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(411, 138);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(213, 24);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Họ tên: Lê Thành Trung";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(102, 184);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(236, 24);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Mã sinh viên: 20103100662";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(411, 184);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(211, 24);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "Họ tên: Nguyễn Anh Tú";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(102, 230);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(236, 24);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "Mã sinh viên: 20103100954";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(411, 230);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(225, 24);
-            this.label9.TabIndex = 7;
-            this.label9.Text = "Họ tên: Phạm Văn Phong";
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 33);
+            this.pnMain.Margin = new System.Windows.Forms.Padding(4);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(772, 387);
+            this.pnMain.TabIndex = 1;
             // 
             // fQuanly
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(772, 420);
-            this.Controls.Add(this.pnThongtinnhaphattrien);
+            this.Controls.Add(this.pnMain);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
             this.Name = "fQuanly";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý thư viện - Trường Đại học Kinh tế Kỹ thuật Công nghiệp";
@@ -313,8 +224,6 @@ namespace QuanLyThuVien
             this.Load += new System.EventHandler(this.fQuanly_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.pnThongtinnhaphattrien.ResumeLayout(false);
-            this.pnThongtinnhaphattrien.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -328,7 +237,7 @@ namespace QuanLyThuVien
         private System.Windows.Forms.ToolStripMenuItem đổiMậtKhẩuToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem đăngXuấtToolStripMenuItem;
-        private System.Windows.Forms.Panel pnThongtinnhaphattrien;
+        private System.Windows.Forms.Panel pnMain;
         private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem quảnLýTàiKhoảnToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem danhMucjToolStripMenuItem;
@@ -339,14 +248,6 @@ namespace QuanLyThuVien
         private System.Windows.Forms.ToolStripMenuItem báoCáoThốngKêToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tácGiảToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem nhàXuấtBảnToolStripMenuItem;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem thôngTinNhómToolStripMenuItem;
     }
 }
---
+++ QuanLyThuVien/ucThongtinnhom.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace QuanLyThuVien
+{
+    partial class ucThongtinnhom
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Location = new System.Drawing.Point(0, 93);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 206);
+            this.panel1.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 24);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Mã sinh viên: 20103100683";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 153);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(236, 24);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Mã sinh viên: 20103100954";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Họ tên: Nguyễn Thiện Thắng";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(324, 153);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(225, 24);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Họ tên: Phạm Văn Phong";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(324, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Họ tên: Lê Thành Trung";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(236, 24);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Mã sinh viên: 20103100662";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Mã sinh viên: 20103100689";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(324, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(211, 24);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Họ tên: Nguyễn Anh Tú";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 37);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Thông tin thành viên - Nhóm 5";
+            // 
+            // ucThongtinnhom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "ucThongtinnhom";
+            this.Size = new System.Drawing.Size(589, 302);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+    }
+}
